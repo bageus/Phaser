480743694fd2056297462336f5c4f15c14fd066e
bageus <36338626+bageus@users.noreply.github.com>
2026-03-28 23:24:15 +0300

    Update TunnelRenderer.js

--- js/phaser/tunnel/TunnelRenderer.js
+++ js/phaser/tunnel/TunnelRenderer.js
@@ -324,8 +324,8 @@ class TunnelRenderer {
 
   drawMouthRing(centerX, centerY, tube) {
     const rimColor = 0xaedcff;
-    const outerRadius = CONFIG.TUBE_RADIUS * 1.3;
-    const innerRadius = CONFIG.TUBE_RADIUS * 1.26;
+    const outerRadius = CONFIG.TUBE_RADIUS * 1.9;
+    const innerRadius = CONFIG.TUBE_RADIUS * 2.26;
     const centerShift = Math.hypot(tube.centerOffsetX || 0, tube.centerOffsetY || 0);
     const shiftBoost = clamp(centerShift / 120, 0, 0.22);
 
